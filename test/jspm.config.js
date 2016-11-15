9fb95d92383934432d7f63bc2fcefae44d88c462
Alexis Vincent <alexisjohnvincent@gmail.com>
2016-11-15 21:45:57 +0200

    Basic path traversal and node conversion

---
+++ test/jspm.config.js
@@ -0,0 +1,34 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "test/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.17"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "test": {
+      "main": "test.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
