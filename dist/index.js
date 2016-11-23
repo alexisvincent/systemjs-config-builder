c839b82e859f347bd27b41ec4bc8b77434c60737
Alexis Vincent <alexisjohnvincent@gmail.com>
2016-11-23 15:08:05 +0200

    Added documentation and fixed windows issues

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", {
  * Copyright 2016 Alexis Vincent (http://alexisvincent.io)
  */
 var fs = require('graceful-fs');
-var path = require('path');
+var path = require('path').posix;
 var Promise = require('bluebird');
 var _ = require('lodash');
 
@@ -51,7 +51,9 @@ var getDirectories = function getDirectories(srcpath) {
  * @returns {Promise.<TResult>}
  */
 var getPackageConfig = exports.getPackageConfig = function getPackageConfig(dir) {
-    return pfs.readFile(path.join(dir, 'package.json'), 'utf8').then(JSON.parse).then(function (config) {
+    return pfs.readFile(path.join(dir, 'package.json'), 'utf8').then(JSON.parse)
+    // Pad it with defaults
+    .then(function (config) {
         return Object.assign({
             dependencies: {},
             devDependencies: {},
@@ -82,7 +84,7 @@ var getOwnDeps = exports.getOwnDeps = function getOwnDeps(packageDir) {
     })
     // Filter out anything that wasn't a package
     .then(function (configs) {
-        return configs.filter(function (k, v) {
+        return configs.filter(function (v, k) {
             return v;
         });
     }).catch(function (err) {
@@ -92,7 +94,30 @@ var getOwnDeps = exports.getOwnDeps = function getOwnDeps(packageDir) {
 };
 
 /**
- * Trace the full node_modules tree.
+ * Trace the full node_modules tree, and build up a registry on the way.
+ *
+ * Registry is of the form:
+ * {
+ *    'lodash@1.1.2': {
+ *      name: 'lodash',
+ *      config: <the package.json file>,
+ *      key: 'lodash@1.1.2',
+ *      location: 'node_modules/lodash'
+ *    },
+ *    ...
+ * }
+ *
+ * Returned Tree is of the form:
+ * [
+ *    {
+ *      name: 'react',
+ *      version: '15.4.1',
+ *      deps: <tree, like this one>
+ *    },
+ *    ...
+ * ]
+ *
+ *
  * @param directory
  * @param name
  * @param version
@@ -102,7 +127,7 @@ var getOwnDeps = exports.getOwnDeps = function getOwnDeps(packageDir) {
 var traceModuleTree = exports.traceModuleTree = function traceModuleTree(directory) {
     var name = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
     var version = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-    var registry = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
+    var registry = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
 
 
     return Promise.resolve({ name: name, version: version })
@@ -157,6 +182,25 @@ var traceModuleTree = exports.traceModuleTree = function traceModuleTree(directo
     });
 };
 
+/**
+ * Take an array of objects and turn it into an object with the key being the specified key.
+ *
+ * objectify('name', [
+ *      {name: 'Alexis', surname: 'Vincent'},
+ *      {name: 'Julien', surname: 'Vincent'}
+ * ])
+ *
+ * =>
+ *
+ * {
+ *    'Alexis': {name: 'Alexis', surname: 'Vincent'},
+ *    'Julien': {name: 'Julien', surname: 'Vincent'},
+ * }
+ *
+ * @param key
+ * @param array
+ * @returns {*}
+ */
 var objectify = function objectify(key, array) {
     return array.reduce(function (obj, arrayItem) {
         obj[arrayItem[key]] = arrayItem;
@@ -164,26 +208,39 @@ var objectify = function objectify(key, array) {
     }, {});
 };
 
+/**
+ * Given a registry of package.json files, use jspm/npm to augment them to be SystemJS compatible
+ * @param registry
+ * @returns {Promise.<TResult>}
+ */
 var augmentRegistry = exports.augmentRegistry = function augmentRegistry(registry) {
     return Promise.all(Object.keys(registry).map(function (key) {
         var depMap = registry[key];
 
+        // Don't augment things that already have been (from the cache)
         var shouldAugment = !depMap.augmented;
 
+        // Don't augment things that specify config.jspmPackage
         if (depMap.config.jspmPackage != undefined && depMap.config.jspmPackage) shouldAugment = false;
 
+        // Don't augment things that specify config.jspmNodeConversion == false
         if (depMap.config.jspmNodeConversion !== undefined && !depMap.config.jspmNodeConversion) shouldAugment = false;
 
+        // Don't augment things that specify config.jspm.jspmNodeConversion == false
         if (depMap.config.jspm !== undefined && depMap.config.jspm.jspmNodeConversion !== undefined && !depMap.config.jspm.jspmNodeConversion) shouldAugment = false;
 
-        console.log(depMap.location, shouldAugment);
-
+        // Augment the package.json
         return shouldAugment ? convertPackage(depMap.config, ':' + key, './' + depMap.location, console).then(function (config) {
             return Object.assign(depMap, { config: config, augmented: true });
         }).catch(log) : depMap;
     })).then(objectify.bind(null, 'key'));
 };
 
+/**
+ * Convenience method to allow easy chaining
+ * @param tree
+ * @param registry
+ */
 var augmentModuleTree = exports.augmentModuleTree = function augmentModuleTree(_ref5) {
     var tree = _ref5.tree,
         registry = _ref5.registry;
@@ -192,6 +249,11 @@ var augmentModuleTree = exports.augmentModuleTree = function augmentModuleTree(_
     });
 };
 
+/**
+ * Only keep keys we are interested in for package config generation
+ * @param registry
+ * @returns {Promise.<*>}
+ */
 var pruneRegistry = exports.pruneRegistry = function pruneRegistry(registry) {
     return Promise.resolve(objectify('key', Object.keys(registry).map(function (key) {
         return Object.assign({}, registry[key], {
@@ -200,6 +262,11 @@ var pruneRegistry = exports.pruneRegistry = function pruneRegistry(registry) {
     })));
 };
 
+/**
+ * Convenience method to allow easy chaining
+ * @param tree
+ * @param registry
+ */
 var pruneModuleTree = exports.pruneModuleTree = function pruneModuleTree(_ref6) {
     var tree = _ref6.tree,
         registry = _ref6.registry;
@@ -209,12 +276,12 @@ var pruneModuleTree = exports.pruneModuleTree = function pruneModuleTree(_ref6) 
 };
 
 /**
- * Walk the tree
+ * Walk the tree, call f on all nodes.
  * @param tree
  * @param registry
- * @param f
- * @param depth
- * @param skip
+ * @param f - (versionName, deps, tree)
+ * @param depth - How deep should we go
+ * @param skip - How many levels should we skip
  */
 var walkTree = exports.walkTree = function walkTree(_ref7, f) {
     var tree = _ref7.tree,
@@ -236,18 +303,31 @@ var walkTree = exports.walkTree = function walkTree(_ref7, f) {
     }
 };
 
+/**
+ * Use the tree and registry to create a SystemJS config
+ *
+ * TODO: Use SystemJS 20 normalize idempotency to optimize mappings
+ * // Do this by mapping package@version to location like JSPM does
+ *
+ * @param tree
+ * @param registry
+ * @returns {Promise.<{map: {}, packages: {}}>}
+ */
 var generateConfig = exports.generateConfig = function generateConfig(_ref8) {
     var tree = _ref8.tree,
         registry = _ref8.registry;
+
 
     var systemConfig = {
         "map": {},
         "packages": {}
     };
 
+    // get readable stream working
+    // TODO: Fix this hack
     systemConfig['map']["_stream_transform"] = "node_modules/readable-stream/transform";
 
-    // Top level maps
+    // Walk first level of dependencies and map package name to location
     walkTree({ tree: tree, registry: registry }, function (_ref9, deps) {
         var name = _ref9.name,
             config = _ref9.config,
@@ -257,7 +337,7 @@ var generateConfig = exports.generateConfig = function generateConfig(_ref8) {
         systemConfig['map'][name] = location;
     }, 2, 1);
 
-    // Walk the others and generate package entries
+    // Walk full dep tree and assign package config entries
     walkTree({ tree: tree, registry: registry }, function (_ref10, deps, tree) {
         var name = _ref10.name,
             config = _ref10.config,
@@ -265,41 +345,47 @@ var generateConfig = exports.generateConfig = function generateConfig(_ref8) {
             location = _ref10.location;
 
 
-        var depMappings = {};
+        // Construct package entry based off config
+        var packageEntry = Object.assign({
+            map: {},
+            meta: {}
+        }, config);
 
+        // Add mappings for it's deps.
         walkTree({ tree: tree, registry: registry }, function (_ref11, deps) {
             var name = _ref11.name,
                 config = _ref11.config,
                 key = _ref11.key,
                 location = _ref11.location;
 
-            depMappings[name] = location;
+            packageEntry['map'][name] = location;
         }, 2, 1);
 
-        var packageEntry = Object.assign({
-            map: {},
-            meta: {}
-        }, config);
-
-        packageEntry['map'] = Object.assign(packageEntry['map'], depMappings);
-
+        // If there are no mappings, don't pollute the config
         if (Object.keys(packageEntry['map']).length == 0) delete packageEntry['map'];
 
+        // Assign package entry to config
         systemConfig['packages'][location] = packageEntry;
 
+        // Add mappings for all jspm-nodelibs
+        // TODO: Fix this hack
         nodeCoreModules.forEach(function (lib) {
             systemConfig['map'][lib] = "node_modules/jspm-nodelibs-" + lib;
         });
     }, Infinity, 1);
 
+    // TODO: Make the mappings here more universal
+    // map nm: -> node_modules/ to make config smaller
     systemConfig['paths'] = {
         'nm:': 'node_modules/'
     };
 
+    // map nm: -> node_modules/ to make config smaller
     Object.keys(systemConfig['map']).forEach(function (key) {
         systemConfig['map'][key] = systemConfig['map'][key].replace(/^node_modules\//, 'nm:');
     });
 
+    // map nm: -> node_modules/ to make config smaller
     Object.keys(systemConfig['packages']).forEach(function (key) {
         if (key.startsWith('node_modules/')) {
             systemConfig['packages'][key.replace(/^node_modules\//, 'nm:')] = systemConfig['packages'][key];
@@ -310,7 +396,7 @@ var generateConfig = exports.generateConfig = function generateConfig(_ref8) {
     return Promise.resolve(systemConfig);
 };
 
-// This needs to be done better (fails if locations of shit changes)
+// TODO: This needs to be done better (fails if locations of shit changes)
 var mergeCache = exports.mergeCache = function mergeCache(registry, cachedRegistry) {
     return Object.assign({}, registry, cachedRegistry);
 };
@@ -324,6 +410,12 @@ var fromCache = exports.fromCache = function fromCache(_ref12) {
     });
 };
 
+/**
+ * Convenience method to allow easy chaining
+ * @param tree
+ * @param registry
+ * @returns {Promise.<{tree: *, registry: *}>}
+ */
 var toCache = exports.toCache = function toCache(_ref13) {
     var tree = _ref13.tree,
         registry = _ref13.registry;
@@ -337,10 +429,19 @@ var serializeConfig = exports.serializeConfig = function serializeConfig(config)
     return 'SystemJS.config(' + JSON.stringify(config, null, 2) + ')';
 };
 
+/**
+ * Write registry to ./systemjs.cache
+ * @param registry
+ * @returns {Promise.<TResult>}
+ */
 var hydrateCache = function hydrateCache(registry) {
     return Promise.resolve(JSON.stringify(registry)).then(pfs.writeFile.bind(null, './systemjs.cache'));
 };
 
+/**
+ * Construct registry from ./systemjs.cache
+ * @returns {Promise.<TResult>}
+ */
 var dehydrateCache = function dehydrateCache() {
     return pfs.readFile('./systemjs.cache', 'utf8').then(JSON.parse).catch(function (e) {
         console.log("No cache, parsing node_modules. Warning this may take a while.");
